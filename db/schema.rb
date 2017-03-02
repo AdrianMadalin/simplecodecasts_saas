d53cc94663e74ca3615f9a06030e280e49367286
adriandinu <adrian.madalin.dinu@gmail.com>
2017-03-02 12:57:12 +0000

    Added contact form and works

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302100113) do
+ActiveRecord::Schema.define(version: 20170302124709) do
 
   create_table "contacts", force: true do |t|
-    t.string "name"
-    t.string "email"
-    t.text   "comments"
+    t.string   "name"
+    t.string   "email"
+    t.text     "comments"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
